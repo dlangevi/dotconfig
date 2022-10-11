29af5148649391b074434325f0c27eebee2541e0
David Langevin <dlangevi@uwaterloo.ca>
2022-10-11 14:26:19 -0700

    Enable a ton of stuff.
    At this point this will be my new setup, already have the funcionality I want

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,15 @@ vim.o.wildmenu = true
 vim.o.mouse = false
 vim.o.nofoldenable = true
 vim.o.noundofile = true
+vim.o.relativenumber = true
+vim.o.nu = true
+
+vim.o.autoindent = true
+vim.o.expandtab = true
+vim.o.smartab = true
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 
 -- Folds
 vim.o.foldmethod = "syntax"
@@ -21,7 +30,31 @@ vim.o.shell = "/usr/bin/bash"
 -- set verbose=1
 vim.o.scrolloff = 10
 
+require('coc')
 require('Navigator').setup()
+require('lualine').setup()
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { 
+    "c", 
+    "lua", 
+    "rust", 
+    "javascript", 
+    "typescript", 
+    "vue" 
+  },
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+          return true
+      end
+    end,
+    additional_vim_regex_highlighting = false,
+  },
+})
+
 
 -- Keybindings
 vim.keymap.set('n', "<c-h>", '<CMD>NavigatorLeft<CR>')
@@ -29,6 +62,10 @@ vim.keymap.set('n', "<c-l>", '<CMD>NavigatorRight<CR>')
 vim.keymap.set('n', "<c-k>", '<CMD>NavigatorUp<CR>')
 vim.keymap.set('n', "<c-j>", '<CMD>NavigatorDown<CR>')
 vim.keymap.set('n', "<c-p>", '<CMD>NavigatorPrevious<CR>')
+vim.keymap.set('n', '--', ':edit<Space>#<cr>')
+
+vim.g.NERDTreeWinPos = "right"
+vim.keymap.set('n', "<leader>t", ':NERDTreeToggle<CR>')
 
 function map(mode, lhs, rhs, opts)
     local options = { noremap = true }
@@ -38,4 +75,11 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map('n', '<leader>f', ':Clap files<CR>')
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fr', builtin.resume, {})
+
+vim.cmd[[colorscheme sonokai]]
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,9 +3,32 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'tpope/vim-surround'
-	use 'liuchengxu/vim-clap'
+	use 'Yggdroot/indentLine'
 	use { 'numToStr/Navigator.nvim',
 	    config = function()
 		require('Navigator').setup()
 	    end }
+	use { 'neoclide/coc.nvim', branch = 'release' }
+	use {
+	  'nvim-lualine/lualine.nvim',
+	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+	use 'scrooloose/nerdtree'
+	use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+  -- Colorschemes
+  use "rafamadriz/neon"
+  use 'sainnhe/sonokai'
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- Training
+  use 'ThePrimeagen/vim-be-good'
 end)
